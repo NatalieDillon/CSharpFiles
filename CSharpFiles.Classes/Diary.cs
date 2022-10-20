d6794ad0fb229ee7f4866cd61400afdce23af72c
TechDomani <natalie.dillon@gmail.com>
2022-10-20 17:31:53 +0100

    Add example code for text and binary files

---
+++ CSharpFiles.Classes/Diary.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFiles.Classes
+{    
+    public class Diary
+    {
+        public record DiaryRecord(DateOnly Date, TimeOnly Time, string Location, string Description, int Duration = 60);
+        public static List<DiaryRecord> ReadDiaryEvents(string fileName)
+        {
+            var diaryEvents = new List<DiaryRecord>();
+            using (var streamReader = new StreamReader(fileName))
+            {
+                while (!streamReader.EndOfStream)
+                {
+                    string line = streamReader.ReadLine() ?? string.Empty;
+                    string[] fields = line.Split(',');
+                    if (fields.Length == 5)
+                    {
+                        if (DateOnly.TryParseExact(fields[0], "yyyyMMdd", out DateOnly date)
+                            && TimeOnly.TryParseExact(fields[1], "Hss", out TimeOnly time)
+                            && int.TryParse(fields[2], out int duration))
+                        {
+                            var diaryEvent = new DiaryRecord(date, time, fields[3], fields[4], duration);
+                            diaryEvents.Add(diaryEvent);
+                        }
+                    }
+                }
+            }
+            return diaryEvents;
+        }
+
+        public static void WriteDiaryEvents(string fileName, List<DiaryRecord> diaryEvents)
+        {
+            using (var streamWriter = File.AppendText(fileName))
+            {
+                foreach (var item in diaryEvents)
+                {
+                    streamWriter.WriteLine($"{item.Date:yyyyMMdd},{item.Time:HHss},{item.Duration},{item.Location},{item.Description}");
+                }                
+            }
+        }
+    }
+}
+
---
+++ CSharpFiles.Classes/Student.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFiles.Classes
+{
+    public class Student
+    {
+        public record StudentRecord(string name, int age);
+
+        public static void WriteToBinaryFile(string fileName, List<StudentRecord> students)
+        {
+            using (BinaryWriter binWriter = new BinaryWriter(File.Open(fileName, FileMode.Create)))
+            {
+                foreach (var student in students)
+                {
+                    binWriter.Write(student.name);
+                    binWriter.Write(student.age);
+                }
+            }
+        }
+
+        public static string ReadBinaryFileBytes(string fileName)
+        {
+            using (BinaryReader reader = new BinaryReader(File.OpenRead(fileName)))
+            {
+                byte[] bytes = reader.ReadBytes((int)reader.BaseStream.Length);
+                return string.Join(',', bytes);
+            }
+        }
+
+        public static List<StudentRecord> ReadBinaryFile(string fileName)
+        {
+            var students = new List<StudentRecord>();
+            using (BinaryReader reader = new BinaryReader(File.OpenRead(fileName)))
+            {
+                while (reader.BaseStream.Position < reader.BaseStream.Length)
+                {
+                    string name = reader.ReadString();
+                    int age = reader.ReadInt32();
+                    students.Add(new StudentRecord(name, age));
+                }
+            }
+            return students;
+        }
+    }
+}
